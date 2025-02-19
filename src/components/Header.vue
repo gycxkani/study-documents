47cb394ca875e1459981ed5f4dad7e687ff4a5cd
gycxkani <454683496@qq.com>
2025-02-19 11:48:19 +0800

    refactor: Remove commented out code and update component import in App.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <el-container style="margin: 0; padding: 0">
+    <el-header style="margin: 0; padding: 0">
+      <div id="title">Vue学习笔记</div>
+    </el-header>
+    <el-main style="margin: 0; padding: 0">
+      <el-menu
+        mode="horizontal"
+        background-color="#e8e7e3"
+        text-color="#777777"
+        active-text-color="#000000"
+        :default-active="0"
+      >
+        <el-menu-item
+          v-for="item in items"
+          :index="item.index"
+          :key="item.index"
+        >
+          <div id="text">{{ item.title }}</div>
+        </el-menu-item>
+      </el-menu>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
+
+<style scoped>
+#title {
+  color: brown;
+  font-size: 40px;
+  font-weight: bold;
+  font-family: Georgia, "Times New Roman", Times, serif;
+}
+#text {
+  font-size: 20px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <el-container id="container">
+    <el-header style="width: 100%" height="120px">
+      <Header :items="navItems"></Header>
+    </el-header>
+    <el-main>
+      <Body :items="bodyItems"></Body>
+    </el-main>
+    <el-footer>
+      <div id="footer">{{ desc }}</div>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Body from "./Body.vue";
+
+export default {
+  components: {
+    Header,
+    Body,
+  },
+  data() {
+    return {
+      navItems: [
+        { index: 0, title: "HTML专题" },
+        { index: 1, title: "CSS专题" },
+        { index: 2, title: "JavaScript专题" },
+        { index: 3, title: "Vue专题" },
+        { index: 4, title: "Element Plus专题" },
+      ],
+      bodyItems: [
+        { index: 0, title: "HTML简介" },
+        { index: 1, title: "HTML编辑器" },
+        { index: 2, title: "JavaScript" },
+        { index: 3, title: "Vue" },
+        { index: 4, title: "Element Plus" },
+      ],
+      desc: "版权所有，仅限学习使用，禁止商业用途",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#container {
+  height: 800px;
+  margin-left: 150px;
+  margin-right: 150px;
+  margin-top: 30px;
+}
+#footer {
+  text-align: center;
+  background-color: bisque;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+  color: #717171;
+}
+</style>
