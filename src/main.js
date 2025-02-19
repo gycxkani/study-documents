0d078e01f5a7d083fa399370142164103031af64
gycxkani <454683496@qq.com>
2025-02-19 10:28:11 +0800

    chore: Update npm dependencies and configure Vue.js options
    - Update npm dependencies to latest stable versions, including @element-plus/icons-vue, axios, and vue-axios
    - Configure Vue.js options in vue.config.js to enable Vue 3 options API and disable production devtools and hydration mismatch details
    - Import and use Element Plus components and icons in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus, { size: "small" }).use(VueAxios,axios);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.mount("#app");
